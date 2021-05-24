3a40c19afbb900e5a73ef9301d3d9f5d9ae2e0a5
HELIOPOTELICKI <helio.p2000@hotmail.com>
2021-05-24 15:32:58 -0300

    nQuenn

---
+++ nQuenn.py
@@ -0,0 +1,25 @@
+'''
+EXERCÍCIO PARCIAL 05 - PROBLEMA N-RAINHAS
+Dupla: Hélio Potelicki e Pedro Henrique Roweder
+
+'''
+from itertools import permutations
+
+N = 8
+sol = 0
+cols = range(sol, N)
+
+for combo in permutations(cols):
+    if N == len(set(combo[i] + i
+                    for i in cols)) == len(set(combo[i] - i for i in cols)):
+        sol += 1
+
+        resp = "\n".join(' 0 ' * i + ' 1 ' + ' 0 ' * (N - i - 1)
+                         for i in combo)
+
+        if (sol == 1):
+            print(f'Inicial --> {combo} \n{resp} \n\n')
+
+        lastCombo = combo
+
+print(f'Final --> {lastCombo} \n{resp} \n\n')
